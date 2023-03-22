fb8d92f28d83c85458b20507d8d839736f114e8a
Empezeeeet <pawelkomateusz@gmail.com>
2023-03-22 15:30:18 +0100

    New Rendering 'engine'

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Direction(Enum):
     DOWN = 1
     LEFT = 2
     RIGHT = 3
+    STAY = 4
 
 
 
@@ -26,37 +27,25 @@ class Renderer(threading.Thread):
         self.fps = kwargs.get("fps", 2)
         pass
     
-    async def render_new_frame(self, snek: list, food: list) -> bool:
+    async def new_frame_renderer(self, snek: list, food: list, points: int = 0, highscore: int = 0):
         """
-            snek: tuple, (x, y)
-            food: tuple, (x, y)
+            snek: list, (x, y)
+            food: list, (x, y)
         """
-        if snek[0] > self.size: 
-            snek[0] = self.size
-        if snek[1] > self.size-1:
-            snek[1] = self.size -2
-        if snek[0] < 1: 
-            snek[0] = 1
-        if snek[1] < 1:
-            snek[1] = 0
-        
-        
-        print("-" + "-" * self.size + "-")
-        
-        for i in range(self.size-2):
-            #if i == snek[1]:
-            #    print("|" + " " * snek[0] + "o" + " " * (self.size - snek[0] - 1) + "|")
-            if i == snek[1] and i == food[1]:
-                print("|" + " " * snek[0] + "o" + " " * (food[0] - snek[0] - 1) + "X" + " " * (self.size - food[0] - 1) + "|")
-            elif i == snek[1]:
-                print("|" + " " * snek[0] + "o" + " " * (self.size - snek[0] - 1) + "|")
-            elif i == food[1]:
-                print("|" + " " * food[0] + "X" + " " * (self.size - food[0] - 1) + "|")
-            else: 
-                print("|" + " " * self.size + "|")
-            
-            
-        print("-" + "-" * self.size + "-")
+        print("-" + "-" * (self.size-2) + "-")
+        try:
+            for i in range(self.size - 2):
+                line_content = [" " for i in range(self.size - 2)]
+                if (snek[1] == i): line_content[snek[0]] = "o"
+                if (food[1] == i): line_content[food[0]] = "X"
+                print("|" + "".join(line_content) + "|")
+        except IndexError:
+            pass 
+        print("-" + "-" * (self.size-2) + "-")   
+        print("Points: " + str(points)) 
+        print("Highscore: " + str(highscore))
+
+    
     def render_game_over_screen(self, points: int, start_time: float):
         os.system("cls")
         print("-" + "-" * self.size + "-")
@@ -66,20 +55,18 @@ class Renderer(threading.Thread):
         print("|" + "    Points: " + str(points))
         print("|" + "    Time: " + str(round(time.time() - start_time, 2)))
         print("-" + "-" * self.size + "-")
+        # Write last score to file  
         with open("last_score.txt", "w") as f:
             f.truncate(0)
             f.write(f"Points: {points}\nTime: {round(time.time() - start_time, 2)}")
 class Player():
     def __init__(self, size: int = 32):
-        self.direction = Direction.UP
+        self.direction = Direction.STAY
         self.position = [1, 1]
         self.renderer = Renderer(size=size, fps=2)
         self.fruit = [15, 15]
         self.points = 0
-    
     def _main(self):
-        
-        
         while True:
             # when the player presses specific arrow key the snek will move in that direction
             if keyboard.is_pressed("up"): self.direction = Direction.UP
@@ -93,63 +80,35 @@ class Player():
             elif self.direction == Direction.LEFT: self.position[0] -= 1
             elif self.direction == Direction.RIGHT: self.position[0] += 1   
             if self.position == self.fruit:
-                self.fruit = [random.randint(2, self.renderer.size-2), random.randint(2, self.renderer.size-2)]
+                self.fruit = [random.randint(4, self.renderer.size-4), random.randint(4, self.renderer.size-4)]
                 self.points += 1
             # if out of bands end game
-            if self.position[0] > self.renderer.size:  
+            if self.position[0] > self.renderer.size - 2:  
                 break
-            if self.position[0] < 0:  
+            if self.position[0] < -1:  
                 break
-            if self.position[1] > self.renderer.size:
+            if self.position[1] > self.renderer.size - 2:
                 break
             if self.position[1] < -1:
                 break
             os.system("cls")
-            asyncio.run(self.renderer.render_new_frame(self.position, self.fruit))
-            time.sleep(self.renderer.fps / 50)
+            asyncio.run(self.renderer.new_frame_renderer(self.position, self.fruit, points=self.points))
+            time.sleep(self.renderer.fps / 30)
         self.renderer.render_game_over_screen(self.points, self.start_time)
     def run(self):
         self.start_time = time.time()
         self._main()
-            
-            
-            
-    
-        
-          
+         
 def main():
-
-    player = Player(size=48)
-    os.system(f"mode {player.renderer.size+3},{player.renderer.size+5}")
-    player.run()
+    while True:
+        player = Player(size=32)
+        os.system(f"mode {player.renderer.size+3},{player.renderer.size+5}")
+        player.run()
+        if (input("Do you want to play again? (y/n): ") == "y"):
+            continue
+        else: 
+            break
+    print("Thanks for playing!")
+    input("Press enter to exit...")
            
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-                
-if __name__ == "__main__":
-    main()
+main()
